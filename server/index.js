4cd3c464bcc25795ec4d4acf3bfcf7f5edc5fe03
Ulysse <ulysse.tallepied@hetic.net>
2016-11-09 17:12:02 +0100

    feat(socketio): mutli devices

--- server/index.js
+++ server/index.js
@@ -2,24 +2,30 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
-var messages = []
+let allMessages = {
+  mainRoom: [],
+  room1: [],
+}
 
 app.get('/notoboard', function(req, res){
   res.sendFile(__dirname + '/notoboard.html')
 })
-
 io.on('connection', function(socket){
-  console.log('a user connected')
+  io.emit('previous messages', allMessages.mainRoom)
   socket.on('chat message', function(msg){
     // Send message to clients
     io.emit('chat message', msg)
-    messages.push(msg)
-    console.log(messages)
+    // Save the message
+    allMessages.mainRoom.push(msg)
   })
 
   socket.on('disconnect', function(){
     console.log('user disconnected')
   })
+})
+
+app.get('/send/:idSender', function(req, res){
+  res.sendFile(__dirname + '/send.html')
 })
 
 http.listen(3000, function(){
