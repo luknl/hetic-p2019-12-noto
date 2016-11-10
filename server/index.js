2cbe0e16e7ada15954006f8c9949511e9ff2d35c
Ulysse <ulysse.tallepied@hetic.net>
2016-11-10 14:40:15 +0100

    feat(socketio): Save new rooms messages, mobile user allowed sending messages to the created room, and a lot more #charlesmangwailoveyou

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const io = require('socket.io')(http)
 
 let allMessages = {
   mainRoom: [],
-  room1: [],
+  room1: [
+    {
+      msg: 'First message, hard written'
+    }
+  ],
 }
 let clients = []
 
@@ -18,37 +22,98 @@ app.get('/send/:idSender', function(req, res){
 
 io.on('connection', function(socket){
   io.emit('previous messages', allMessages.mainRoom)
-  socket.on('chat message', function(msg){
-    // Send message to clients
-    io.emit('chat message', msg)
-    allMessages.mainRoom.push(msg)
+  io.emit('previous rooms', Object.keys(allMessages))
+
+  socket.on('chat message', function(data){
+    if (data.roomId == '') {
+      // Send message to clients
+      io.emit('chat message', {
+        msg: data.msg,
+        roomId: data.roomId,
+        roomName: data.roomName,
+      })
+    } else {
+      // Send message to clients
+      io.sockets.in(data.roomId).emit('chat message', {
+        msg: data.msg,
+        roomId: data.roomId,
+        roomName: data.roomName,
+      })
+    }
+    // Check if the message is for the mainRoom
+    if (data.roomId == '') {
+      allMessages.mainRoom.push({msg: data.msg})
+    } else {
+      // Save the message to the good room
+      allMessages[data.roomId].push({msg: data.msg})
+    }
+    console.log(allMessages)
   })
-  socket.on('new user', function(userId){
+
+  socket.on('new user', function(urlId){
     // Save the message
     clients.push({
       socketId: socket.id,
-      id: userId,
+      urlId: urlId,
       time: Date.now(),
     })
   })
 
-  socket.on('join room', function(roomName){
-    socket.join(roomName)
-    io.sockets.in(roomName).emit('chat message', 'what\'s up bitches? ' + roomName)
+  socket.on('join room', function(roomId){
+    if (!allMessages[roomId]) {
+      allMessages[roomId] = []
+    }
+    socket.join(roomId)
+    io.sockets.in(roomId).emit('chat message', {
+      msg: 'what\'s up bitches? ' + roomId,
+      roomId,
+    })
+    // This will be deleted
+    allMessages[roomId].push({msg: 'what\'s up bitches? ' + roomId})
+    console.log(allMessages)
   })
-  // socket.on('leave room', function(roomName){
-  //   socket.leave(roomName)
-  // })
+
+  // Leave
+  socket.on('leave room', function(roomId){
+    socket.leave(roomId)
+  })
+
+
+  // Get user infos to the send page
+  socket.on('get user', function(urlId){
+    const infos = clients.filter(function(el) {
+      return el.urlId == urlId
+    })
+    if (infos.length == 0) {
+      io.emit('user infos', 'Not a valid link')
+    } else {
+      io.emit('user infos', infos)
+    }
+  })
+
+  // Create new room from "send" client
+  socket.on('create room', function(data){
+    // Create memory location where messages will be saved
+    if (!allMessages[data.roomId]) {
+      allMessages[data.roomId] = []
+    }
+    socket.join(data.roomId)
+    io.sockets.in(data.roomId).emit('chat message', {
+      roomId: data.roomId,
+      roomName: data.roomName,
+      msg: 'This is a message to ' + data.roomId,
+    })
+
+    // Create the div for other to join
+    io.emit('add room', {
+      roomId: data.roomId,
+      roomName: data.roomName,
+    })
+    console.log(allMessages)
+  })
 
   socket.on('disconnect', function(){
     console.log('user disconnected')
-  })
-
-  socket.on('get user', function(userId){
-    const infos = clients.filter(function(el) {
-      return el.id == userId
-    })
-    io.emit('user infos', infos)
   })
 })
 
