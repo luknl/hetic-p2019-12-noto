e9017d97ae8543fb9e18c5b0be871246e79b3db1
Luca <haenell@yahoo.fr>
2016-11-15 22:24:05 +0100

    feat(Travis CI) : add build testing

--- src/js/home.js
+++ src/js/home.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const body        = document.querySelector('body'),
-        tofuTitle   = document.querySelector('.tofu__title'),
+        tofuTitle   = document.querySelector('.tofu__title h2'),
         tofuInput   = document.querySelector('.tofu__input'),
-        tofuSpan    = document.querySelector('.tofu__span'),
-        tofuButton  = document.querySelector('.tofu__button'),
-        tofuButtons  = document.querySelector('.tofu__buttons'),
-        tofuButton2  = document.querySelector('.tofu__button2__up'),
-        tofuButton3  = document.querySelector('.tofu__button2__bottom'),
-        tofuButtonFill  = document.querySelector('.tofu__button2__fill'),
-        output = document.querySelector('.lol'),
+        tofuTypeBar    = document.querySelector('.tofu__input__typebar'),
+        tofuSpaceBar  = document.querySelector('.tofu__spacebar'),
+        tofuSpaceBarBtn  = document.querySelector('.tofu__spacebar__button'),
+        tofuSpaceBarDown  = document.querySelector('.tofu__spacebar__button--down'),
+        tofuSpaceBarFill  = document.querySelector('.tofu__spacebar__button_fill'),
         tofu        = [],
         word        = []
   // incrementator for each character
@@ -16,17 +14,17 @@ document.addEventListener('DOMContentLoaded', () => {
   // check if all characters have been revealed
       revealed = false
 
+  // when user starts typing
   body.addEventListener('keyup', (e) => {
     // user is only allowed to type letters
     if (e.keyCode >= 65 && e.keyCode <= 90){ //  || e.keyCode === 32
       // no more tofu button appears
-      tofuSpan.style.display   = 'none'
-      tofuTitle.style.display  = 'none'
-      // tofuButton.style.display = 'block'
-      tofuButtons.style.display = 'block'
-      tofuButton2.innerText = 'Press space to noto'
+      tofuTypeBar.style.display   = 'none'
+      // tofuSpaceBar.style.display = 'block'
+      tofuSpaceBar.style.display = 'flex'
+      tofuSpaceBarBtn.innerText = 'Press space to noto'
       // Add a tofy character for each key press
-      tofu.push(`<span class="tofu__character tofu__character__${i}">□</span>`)
+      tofu.push(`<span class="tofu__input__characters tofu__input__character__${i}">□</span>`)
       // Add pressed key character in "word" array
       word.push(e.key.toUpperCase())
       tofuInput.innerHTML += tofu[i]
@@ -37,78 +35,60 @@ document.addEventListener('DOMContentLoaded', () => {
       tofu.pop()
       word.pop()
       tofuInput.innerHTML = tofu.join('')
+      i--
     }
   })
 
-  // tofuButton.addEventListener('click', () => {
-  //   for (let i in word) {
-  //     const characters = document.querySelector(`.tofu__character__${i}`)
-  //     setTimeout(() => {
-  //       characters.innerHTML = word[i]
-  //       characters.className += " tofu__animation"
-  //     }, i * 400)
-  //   }
-  // })
-
-
-
+  // when user press spacebar
   body.addEventListener('keydown', (e) => {
 
-    if(!revealed){ // PAS SUR
+    // if(!revealed){ // PAS SUR
 
       if (e.keyCode === 32) {
         console.log('space down!')
-        tofuButton3.style.display = 'none'
-        tofuButton2.innerText = 'reveal in progress...'
-        // add class animation to fill button
-        tofuButtonFill.className = 'tofu__button2__fill animation'
-        tofuButtonFill.style.transitionDuration = (i * .4)+'s'
-
-        // if (e in pressed) return
-        // pressed[e] = e.timeStamp
+        tofuSpaceBarDown.className = 'tofu__spacebar__button'
+        tofuSpaceBarBtn.style.marginTop = '10px'
+        tofuSpaceBarBtn.innerText = 'reveal in progress...'
+        // add class fillAnimation to fill button as characters are revealed
+        tofuSpaceBarFill.className = 'tofu__spacebar__button_fill fillAnimation'
+        tofuSpaceBarFill.style.transitionDuration = (i * .4)+'s'
 
         // transform one by one each tofu into characters
         for (let i in word) {
-          const characters = document.querySelector(`.tofu__character__${i}`)
+          const characters = document.querySelector(`.tofu__input__character__${i}`)
           setTimeout(() => {
             characters.innerHTML = word[i]
             characters.className += " tofu__animation"
           }, i * 400)
         }
         // after all letters have been revealed
-        setTimeout(() => {
-          console.log('yes!')
-          tofuButton2.innerText = 'ta-dah! now release.'
-          revealed = true
-          tofuInput.innerHTML = ''
-
-          body.addEventListener('keyup', (e) => { // PAS SUR
-            if (e.keyCode === 32) {
-              // tofuButtons.display = 'none'
-            }
-          })
-
-        }, i * 400)
+        // setTimeout(() => {
+        //   console.log('yes!')
+        //   tofuSpaceBarBtn.innerText = 'ta-dah! now release.'
+        //   revealed = true
+        //   tofuInput.innerHTML = ''
+        //
+        //   body.addEventListener('keyup', (e) => { // PAS SUR
+        //     if (e.keyCode === 32) {
+        //       // tofuSpaceBar.display = 'none'
+        //     }
+        //   })
+        //
+        // }, i * 400)
       }
 
-  }
+  // }
 
-    // releasing the spacebar
+    // when user relase the spacebar
     body.addEventListener('keyup', (e) => {
       if (e.keyCode === 32) {
         console.log('space up')
-        tofuButton3.style.display = 'flex'
-        tofuButton2.innerText = 'Press space to noto'
-        tofuButtonFill.className = 'tofu__button2__fill'
+        tofuSpaceBarDown.className += ' tofu__spacebar__button--down'
+        tofuSpaceBarBtn.innerText = 'Press space to noto'
+        tofuSpaceBarBtn.style.marginTop = '0px'
+        tofuSpaceBarFill.className = 'tofu__spacebar__button_fill'
         tofuInput.innerHTML = tofu.join('')
       }
-        // calculate how long the key was pressed
-        // if (!(e in pressed)) return;
-        // let duration = ( e.timeStamp - pressed[e] );
-        // console.log(duration);
-        // test.splice(0);
-        // test.push(duration)
-        // delete pressed[e];
     })
   })
 })
