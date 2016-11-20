2c6e1a1ea5033b97f0451b33ee95f88e264d987c
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-11-20 01:35:24 +0100

    fix(socket): fix last merging

--- src/js/bundles/index.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-
-import { notoBoard } from '@modules'
-import { compose } from '@utils/compose'
-import { getPage } from '@helpers/app'
-
-export const notoSpace = compose(notoSpace)({ page: 'notoboard' })
--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-/* @@flow */
-
-// import { run, getPage } from '@helpers/app'
-// import * as modules from '.@modules'
-//
-// const page = getPage(document)
-// run(page, bundles)
