c92306373d59a47384bdca334d496d047a3128a6
Ulysse <ulysse.tallepied@hetic.net>
2016-11-21 15:40:40 +0100

    feat(notospace): load as module, detect country typing

---
+++ src/client/js/modules/space/index.js
@@ -0,0 +1,190 @@
+/* @flow */
+
+import moment from 'moment'
+import io from 'socket.io-client'
+import { flag } from 'country-code-emoji'
+import { SOCKET_URL } from '@shared/config'
+import { dispatch, watch } from '@shared/helpers/socket'
+import * as actions from '@shared/modules/notoSpace/actions'
+import type { User, Room } from '@shared/modules/notoSpace/types'
+
+export default () => {
+
+    // Initialyze sockets
+    const socket = io(SOCKET_URL)
+
+    // Get actions and actionTypes
+    const { joinRoom, generateUser, ...actionTypes } = actions
+
+    // Select DOM elements
+    const $messages: HTMLElement = document.querySelector('.messages')
+    const $rooms: HTMLElement = document.querySelector('.rooms')
+    const $user: HTMLElement = document.querySelector('.user')
+    const $writers: HTMLElement = document.querySelector('.writers')
+
+    // Initialyze colors
+    const COLORS: Array<string> = ['#457DF3', '#FC1B1F', '#1AB05A', '#FDBD2C']
+
+    // Initialyze state
+    type State = {
+      rooms: Array<Room>,
+      user?: User,
+      countries: {},
+    }
+    const state: State = {
+      rooms: [],
+      countries: {},
+    }
+
+
+    /**
+     * Generate an userId
+     */
+    const language = navigator.language || navigator.userLanguage
+    dispatch(generateUser(language))(socket)
+
+
+    /**
+     * Listen socket events with custom watch
+     * helper
+     */
+    watch(({ type, payload }) => {
+      console.log({ type, payload })
+      switch (type) {
+
+        /**
+         * Add a user
+         */
+        case actionTypes.ADD_USER: {
+          if (!state.user) {
+            const { user } = payload
+            $user.innerHTML = `
+              <li>userId: ${user.id}</li>
+              <li>roomId: ${user.roomId}</li>
+            `
+            state.user = user
+          }
+          break
+        }
+
+
+        /**
+         * Receive a message
+         */
+        case actionTypes.SEND_MESSAGE: {
+          const { message } = payload
+          // Security: check room
+          if (message.roomId !== state.user.roomId) return
+          // Display new message on wall
+          const randomColor = COLORS[Math.floor(Math.random() * 4)]
+          $messages.innerHTML += `
+            <li
+              style="background-color: ${randomColor}"
+              class="messages__single"
+              data-date="from ${message.country} at ${moment(message.createAt).format('h:mm a')}"
+            >
+              ${message.value}
+            </li>
+          `
+          break
+        }
+
+
+        /**
+         * Receive all messages
+         */
+        case actionTypes.GET_ALL_MESSAGES: {
+          const { messages } = payload
+          // Display all message on wall
+          const randomColor = COLORS[Math.floor(Math.random() * 4)]
+          $messages.innerHTML = messages.map((message) => `
+            <li
+              style="background-color: ${randomColor}"
+              class="messages__single"
+              data-date="from ${message.country} at ${moment(message.createAt).format('h:mm a')}"
+            >
+              ${message.value}
+            </li>
+          `).join('')
+          state.messagesAreInitialized = true
+          return
+        }
+
+
+        /**
+         * Receive all rooms
+         */
+        case actionTypes.GET_ALL_ROOMS: {
+          // Display all rooms on wall
+          const { rooms } = payload
+          state.rooms = rooms
+          $rooms.innerHTML = rooms.map((room) => `
+            <div id="${room.id}" class="rooms__single">${room.name}</div>
+          `).join('')
+          return
+        }
+
+
+        /**
+         * Add new room
+         */
+        case actionTypes.CREATE_ROOM: {
+          const { room } = payload
+          $rooms.innerHTML += `
+            <div id="${room.id}" class="rooms__single">
+              ${room.name}
+            </div>
+          `
+          break
+        }
+
+        /**
+         * Join a room
+         */
+        case actionTypes.JOIN_ROOM: {
+          const { user, room } = payload
+          if (state.user.id !== user.id ) return
+          // Connect desktop to room
+          dispatch(joinRoom(room, user))(socket)
+          // Update state and UI
+          state.user.roomId = user.roomId
+          $messages.innerHTML += ''
+          $user.innerHTML = `
+            <li>id: ${user.id}</li>
+            <li>roomId: ${user.roomId}</li>
+          `
+          break
+        }
+
+        case actionTypes.START_TYPING: {
+          const { user } = payload
+          if (!state.countries[user.language] ||!document.querySelector(`#${user.language}`)) {
+            state.countries[user.language] = { count: 1 }
+            $writers.innerHTML += `
+              <ul
+                id="${user.language}"
+                class="writers__single ${user.language}"
+                data-count="${state.countries[user.language].count}">
+                  ${flag(user.language.slice(-2))}
+              </ul>
+            `
+          } else {
+            state.countries[user.language].count++
+            document.querySelector(`.${user.language}`).setAttribute('data-count', state.countries[user.language].count)
+          }
+          break
+        }
+
+        case actionTypes.STOP_TYPING: {
+          const { user } = payload
+          state.countries[user.language].count--
+          if (state.countries[user.language].count === 0 && document.querySelector(`#${user.language}`)) {
+            document.querySelector(`#${user.language}`).outerHTML = ''
+          }
+          break
+        }
+
+      }
+    })(socket)
+
+}
