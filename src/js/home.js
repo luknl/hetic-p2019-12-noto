a22ef16a641687e3c15259b13151442e3d18af03
Luca <haenell@yahoo.fr>
2016-11-21 18:32:08 +0100

    feat(home_page): added mobile compatibility

--- src/js/home.js
+++ src/js/home.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const body        = document.querySelector('body'),
-        tofuTitle   = document.querySelector('.tofu__title h2'),
-        tofuInput   = document.querySelector('.tofu__input'),
+  const body         = document.querySelector('body'),
+        tofuContent  = document.querySelector('.tofu'),
+        tofuTitle    = document.querySelector('.tofu__title h2'),
+        tofuInput    = document.querySelector('.tofu__input'),
+        tofuInputVal = document.querySelector('.tofu__input input'),
         tofuTypeBar    = document.querySelector('.tofu__input__typebar'),
         tofuSpaceBar   = document.querySelector('.tofu__spacebar'),
         tofuSpaceBarBtn    = document.querySelector('.tofu__spacebar__button'),
@@ -9,26 +11,40 @@ document.addEventListener('DOMContentLoaded', () => {
         tofuSpaceBarFill   = document.querySelector('.tofu__spacebar__button_fill'),
         tofuSpaceBarCircle = document.querySelector('.tofu__spacebar__circle'),
         svgCircle   = document.querySelector('animateTransform'),
+        letterContent = document.querySelector('.letter'),
         tofu        = [],
         word        = [],
         colors      = ['#FF1407','#FFBE00','#00B156','#4179F7'],
-        audio = new Audio('../sounds/loading.aif.m4a')
-  let i        = 0, // incrementator for each character
-      revealed = false, // check if all characters have been revealed
-      fired = false
+        audio = new Audio('../sounds/loading.aif.m4a'),
+        mobile = body.clientWidth < 500
+  let   i        = 0, // incrementator for each character
+        revealed = false, // check if all characters have been revealed
+        fired    = false
+
+  // animating title when page has loaded
+  tofuTitle.style.letterSpacing = '0px'
+  tofuTitle.style.opacity = '1'
+  tofuTitle.style.transition = 'all 2.7s ease'
 
   // when user starts typing
-  body.addEventListener('keyup', (e) => {
+  addEventListeners([body, tofuTypeBar],['keyup', 'touchstart'], (e) => {
+    // for android chrome keycode fix
+    let androidKey = e.keyCode
+    if (androidKey == 0 || androidKey == 229) { // between 29 and 54 for android
+        androidKey = getKeyCode(tofuInputVal.value)
+    }
     // user is only allowed to type letters
-    if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const onlyLetters = mobile ? androidKey >= 65 && androidKey <= 90 : e.keyCode >= 65 && e.keyCode <= 90
+    if (onlyLetters) {
+
       // remove type bar and add bottom space bar
       tofuTypeBar.style.display   = 'none'
-      tofuSpaceBar.style.display = 'flex'
+      tofuSpaceBar.style.visibility = 'visible'
       tofuSpaceBarFill.innerText = 'Press space to noto'
       // Add a tofu character for each key pressed in "tofu" array
       tofu.push(`<span class="tofu__input__characters tofu__input__character__${i}">□</span>`)
       // Add pressed key character in "word" array
-      word.push(e.key.toUpperCase())
+      word.push(String.fromCharCode(androidKey))
       // display characters in the dom
       tofuInput.innerHTML += tofu[i]
       i++
@@ -43,9 +59,9 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   // when user press spacebar
-  body.addEventListener('keydown', (e) => {
+  addEventListeners(mobile ? tofuSpaceBarFill : body, [mobile ? 'touchstart' : 'keydown'], (e) => {
     // only if space key is pressed and at least a characters has been typed
-    if (e.keyCode === 32 && (i != 0)) {
+    if (e.keyCode === 32 && (i != 0) || mobile) {
 
       // triger following events only once while pressing spacebar
       if(!fired) {
@@ -53,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
         fired = true
         // play the audio while space bar is down
         audio.play()
-        audio.volume=0.3
+        audio.volume = 0.3
         audio.currentTime = 1.4
 
         // transform one by one each tofu into characters
@@ -61,6 +77,9 @@ document.addEventListener('DOMContentLoaded', () => {
           let characters = document.querySelector(`.tofu__input__character__${i}`)
           setTimeout(() => {
             characters.innerHTML = word[i]
+            // change color of characters by random colors of the google logo
+            let randomColor = Math.floor(Math.random()*colors.length)
+            characters.style.color = colors[randomColor]
             // add class to animte the transformation from tofu to character
             characters.className += ` tofu__animation`
           }, i * 400)
@@ -69,7 +88,6 @@ document.addEventListener('DOMContentLoaded', () => {
         tofuSpaceBarDown.className = 'tofu__spacebar__button'
         tofuSpaceBarBtn.style.marginTop = '10px'
         tofuSpaceBarFill.innerText = 'reveal in progress...'
-        // tofuSpaceBarFill.style.color = 'white'
         // add class fillAnimation to fill button as characters are revealed
         tofuSpaceBarFill.className = 'tofu__spacebar__button_fill fillAnimation'
         tofuSpaceBarFill.style.transitionDuration = (i * .4)+'s'
@@ -77,13 +95,6 @@ document.addEventListener('DOMContentLoaded', () => {
         // when all characters have been revealed
         setTimeout(() => {
           revealed = true
-
-          // change color of characters by random colors of the google logo
-          for (let i in word) {
-            let characters = document.querySelector(`.tofu__input__character__${i}`)
-            let randomColor = Math.floor(Math.random()*colors.length)
-            characters.style.color = colors[randomColor]
-          }
           tofuSpaceBarFill.innerText = 'ta-dah! now release.'
           // tofuInput.innerHTML = ''
         }, i * 400)
@@ -92,8 +103,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // when user relase the spacebar
-    body.addEventListener('keyup', (e) => {
-      if (e.keyCode === 32) {
+      addEventListeners(mobile ? tofuSpaceBarFill : body, [mobile ? 'touchend' : 'keyup'], (e) => {
+      if (e.keyCode === 32 || mobile) {
         console.log('space up')
         fired = false
 
@@ -104,30 +115,45 @@ document.addEventListener('DOMContentLoaded', () => {
         tofuSpaceBarFill.className = 'tofu__spacebar__button_fill'
         tofuInput.innerHTML = tofu.join('')
 
-        // if all all letters have been revealed
-        if(revealed) {
-
-          // function to animate a growing circle in svg
-          circleGrowth()
-
-          // release sound
-          audio.currentTime = 4.73
-        }
-        else{
-          audio.pause()
-        }
+        // if all letters have been revealed
+        if (revealed) circleGrowth() // function to animate a growing circle in svg
+        else audio.pause()
       }
     })
   })
 
+
   // Functions //
 
+  // function to listen to multiple events on multiple selects
+  // used here to have event listeners make the same function in desktop and mobile
+  function addEventListeners(select, events, callback) {
+    events.forEach((event, index) => {
+      if (Array.isArray(select)) select[index].addEventListener(event, callback)
+      else select.addEventListener(event, callback)
+    })
+  }
+
+  // convert keycode for Android
+  function getKeyCode(str) {
+    return str.charCodeAt(str.length - 1);
+  }
+
+  // svg circle animation function
   function circleGrowth() {
     body.style.overflow = 'hidden'
     tofuSpaceBarCircle.style.display = 'block'
-    tofuSpaceBarFill.innerHTML = 'Wouhou'
+    tofuSpaceBarCircle.style.opacity = '1'
+    // release sound
+    audio.currentTime = 4.73
     // launch svg animation
     svgCircle.beginElement()
+
+    // clean dom when animation is complete
+    setTimeout(() => {
+      tofuContent.remove()
+      letterContent.style.display = 'flex'
+    }, 800)
   }
 
 })
